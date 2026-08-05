a11785dacecf739c2c8cd02c31fe883245412747
Nama Akun Torresndau <email_torresndau_anda@gmail.com>
2026-08-05 15:48:27 +0800

    fix: ubah import path firebase ke services/db

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { auth } from '../services/db';
 import { useNavigate } from 'react-router-dom';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase'; // Diperbaiki dari '../firebase' menjadi '../../firebase' jika di dalam pages/
